a99c07c1b192a42e54a3ca65c2fca51dc8c85f60
johntylerfranklin <johntylerfranklin@gmail.com>
2020-05-28 20:04:56 -0700

    check in

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatMenuModule } from '@angular/material/menu';
@@ -7,12 +7,49 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AuthService } from './services/auth/auth.service';
+import { DebugElement, Component } from '@angular/core';
+import { of } from 'rxjs';
+import { by } from 'protractor';
 
 
 describe('AppComponent', () => {
+  // include the below three lines at top of describe so you don't have to initialize in each spec.
+  // let component: AppComponent;
+  // let fixture: ComponentFixture<AppComponent>;
+  // let de: DebugElement;
+
+  // needed when mocking a service via creating a stub.
+  // let serviceStub: any;
+
+  // needed when testing with live service and spy.
+  // let service: AuthService;
+  // let spy: jasmine.Spy;
+
+  // let fixture: ComponentFixture<AppComponent>;
+  // let de: DebugElement;
   beforeEach(async(() => {
+
+
+    // This is how you instantiate your service for the test.
+    // service = de.injector.get(AuthService);
+
+    // mocking a serivice.  Configure the method to return what the service would return
+    // for instance, getContent() could be login() or logout()
+    // Below is when using a stub
+    // serviceStub = {
+    //   getContent: () => of('You have been warned'),
+    // };
+    // Below is when using the actual service
+    // getContent will be the name of the function that you want to monitor
+    // so for us it would be login or logout.
+    // spy = spyOn(service, 'getContent').and.returnValue(of('Whatever you expect'));
+
     TestBed.configureTestingModule({
-      providers: [ ],
+      // How to correctly add a service/stub to provider for testing
+      // Below is example of stub.
+      // providers: [ { provide: AuthService, useValue: serviceStub} ],
+      // providers: [ { AuthService } ],
       imports: [
         RouterTestingModule,
         MatMenuModule,
@@ -24,12 +61,18 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ]
+      // Compiles the components html and css
     }).compileComponents();
   }));
 
+  // I am going to explain the different elements below.
+  // Each test is refered to as a 'spec'
   it('should create the app', () => {
+    // fixture is the test environment for this component
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    // rendered html.
+    // const de = DebugElement;
     expect(app).toBeTruthy();
   });
 
@@ -39,10 +82,45 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ncaa-fame');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('ncaa-fame app is running!');
+  // See the stub above to make sense of the test.  I will need to model the test to my service
+  // it('should return user login status from an observable', () => {
+  //   component.content.subscribe(content => {
+  //     expect(content).toBeDefined();
+  //     expect(content).toBe('You have been warned');
+  //   });
+  // });
+
+  // below is an example of how to toggle a button
+  // You could replace hide content with a function name like logout or login
+  // it('should toggle the button', () => {
+  //   expect(component.hideContent).toBeTruthy();
+  //   component.toggle();
+  //   expect(component.hideContent).toBeFalsy();
+  // });
+
+  // below is an example of how to toggle a button async
+  // You could replace hide content with a function name like logout or login
+  // it('should toggle the button asynchronously', fakeAsync() => {
+  //   expect(component.hideContent).toBeTruthy();
+  //   component.toggleAsync();
+  //   expect(component.hideContent).toBeFalsy();
+  // });
+
+  // below is an example of how to test a service from a component with spy
+  // it('should call "put function name" one time and update the view', () => {
+    // fixture is the test environment for this component
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const app = fixture.debugElement.componentInstance;
+    // rendered html.
+    // const de = DebugElement;
+    // Calls the spy and the method you set in the spy constructor
+    // expect(spy).toHaveBeenCalled();
+
+    // .calls.all tells how many times the spy is called.
+    // expect(spy.calls.all.length).toEqual(1);
+
+    // Below is how you look into an html element at the inner text.
+    // expect(de.query(by.css('.message-body')).nativeElement.innerText)
+    //   .toContain('warn');
   // });
 });
--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,27 +1,84 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatMenuModule } from '@angular/material/menu';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
+import { AppComponent } from 'src/app/app.component';
+
+// Arrange
+// Act
+// Assert
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ AuthService ],
-    imports: [
-      HttpClientTestingModule,
-      MatMenuModule,
-      AngularFireModule.initializeApp(environment.firebaseConfig),
-      AngularFireDatabaseModule,
-      AngularFireAuthModule
-     ]
-  }));
+  let service: AuthService;
 
-  it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
+  // dependencies that AuthSerive need
+  let db: AngularFireDatabase;
+  let firebaseAuth: AngularFireAuth;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule( { providers: [AuthService] } );
+    // service = new AuthService(db, firebaseAuth); });
   });
-
+  it('#loginUser should return value from promise',
+    (done: DoneFn) => {
+      // service = TestBed.inject
+      service.loginUser('johndoe@gmail.com', 'password')
+      .then(value => {expect(value).toBe(firebaseAuth.authState);
+                      done();
+    });
+    });
 });
+
+
+// TODO********* UNCOMMENT OUT THE BELOW WHEN DONE TESTING
+// describe('AuthService', () => {
+
+//   let service: AuthService;
+//   let spy: jasmine.Spy;
+
+//   beforeEach(() => {
+//     // spy = spyOn(service, 'loginUser').arguments('johndoe@gmail.com', 'password').and.returnValue(null);
+
+//     TestBed.configureTestingModule({
+//     providers: [ AuthService ],
+//     imports: [
+//       HttpClientTestingModule,
+//       MatMenuModule,
+//       AngularFireModule.initializeApp(environment.firebaseConfig),
+//       AngularFireDatabaseModule,
+//       AngularFireAuthModule
+//      ]
+//     });
+//   });
+
+//   it('should use AuthService', () => {
+//     service = TestBed.get(AuthService);
+//     expect(service.loginUser())
+//   });
+
+//   it('should be created', () => {
+//     // const service: AuthService = TestBed.get(AuthService);
+//     const authService: AuthService = TestBed.get(AuthService);
+//     expect(authService).toBeTruthy();
+//   });
+
+//   it('true should be true', () => {
+//     expect(true).toBe(true);
+//   });
+
+//   // Arrange
+//   // Act
+//   // Assert
+
+//   it('loginUser should not return an error message', () => {
+//     const fixture = TestBed.createComponent(AuthService);
+
+//     const errorMessage = 'Not null';
+//     expect(errorMessage).toBe(null);
+//   });
+// });
